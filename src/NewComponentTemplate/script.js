09ed726a56ff8916f9e9da633f4db1cd9895d6de
faidon <faidon.kourounakis@gmail.com>
2019-11-11 19:01:13 +0200

    finished router

---
+++ src/NewComponentTemplate/script.js
@@ -0,0 +1,4 @@
+//import style:
+import './style.scss'
+import template from './template.html'
+
--- src/scripts/router/router.js
+++ src/scripts/router/router.js
@@ -2,37 +2,47 @@ import { dom, fn } from '../store'
 import routes from './routes'
 
 
-window.addEventListener( 'onload', e => {
-    let path = window.location.pathname
-    router( {url: path} ) 
-})
+const router = url => {
 
-const router = route => {
-    //sets the current route
-    let currentRoute
-    if ( route.name && routes.find( currRoute => currRoute.name == route.name ) ) { //checks if the name is given and if it exists
-        currentRoute = routes.find( currRoute => currRoute.name == route.name )
-    } else if ( route.url && routes.find( currRoute => currRoute.url == route.url ) ) {
-        currentRoute = routes.find( currRoute => currRoute.url == route.url )
-    } 
+    if ( routes.find( currRoute => currRoute.url == url ) ) { //checks if the route exist
+        
+        let currentRoute = routes.find( currRoute => currRoute.url == url ) // finds the requested route object
 
-    //adds the routes url to the history
-    window.history.pushState( 
-        {}, //state object in order to be able to retrieve the route
-        `visited the ${currentRoute.name} section`, // the message | idk where this is used but i added it
-        window.location.origin + currentRoute.url // and finally the url is given
-    )
+        //adds the routes url to the history
+        window.history.pushState( 
+            {url: url}, //state object in order to be able to retrieve the route
+            currentRoute.name, // the message | idk where this is used but i added it
+            url // and finally the url is given
+        )
 
-    // changes the router view
-    dom.routerView.innerHTML = currentRoute.component
+        // changes the router view
+        dom.routerView.innerHTML = currentRoute.component
+    } else {
+        alert( 'Invalid path. Redirected to home page' ) 
+        router( '/' )
+    }
 }
 export default router
 
-//set up links and set the landing page to home:
-for ( let route of routes ) {
-    route.link.addEventListener( 'click', () => router( route ) );
-}
-router( { name: 'home' } )
-console.log(window)
-window.addEventListener('popstate', ()=>console.log('popstate'))
-window.addEventListener('onload', ()=>console.log('popstate'))
+
+// sets up the homepage when first loading website
+window.addEventListener( 'load', e => {
+    router( '/' ) 
+})
+// changes route when back button is clicked (onpopstate)
+window.addEventListener( 'popstate', e => {
+    let path = window.location.pathname
+    let url = e.state.url
+    router( url ) 
+})
+
+
+let routerLinks = Array.from(
+    document.querySelectorAll( '[route]' )
+)
+// console.log(routerLinks[0].attributes)
+routerLinks.forEach( el => {
+    el.addEventListener( 'click', e => {
+        router( el.attributes.route.value )
+    } )
+} )

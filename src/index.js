4886cfb7769ed28c5dca4f26ff22d08285e98485
faidon <faidon.kourounakis@gmail.com>
2019-11-11 20:24:37 +0200

    deleted everything

--- src/index.js
+++
@@ -1,7 +0,0 @@
-// IMPORT THE SASS WITH WEBPACK
-// !!! it is needed in order to load the styles !!!
-import "./sass/main.scss"
-
-// IMPORT THE JAVASCRIPT
-// importing it will make it run automatically
-import './scripts/main.js'
--- src/scripts/pages/homePage/homePage.js
+++
@@ -1,7 +0,0 @@
-import './style.scss'
-import template from './template.html'
-
-//code 
-
-
-export default template
